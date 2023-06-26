981ccf19cc759664c4c1f91ed76e3706e6aa5fb8
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-06-26 14:39:54 +0530

    initial commit

--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
   const apiUrl = process.env.REACT_APP_API_URL;
   const submitForm = async (e) => {
     e.preventDefault();
@@ -11,13 +13,17 @@ function RegisterPage() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
     });
-    if(api.status===200){
+    if (api.status === 200) {
       alert("Registration Successul");
-    }
-    else{
+      setRedirect(true);
+    } else {
       alert("Registration failed");
     }
   };
+
+  if (redirect) {
+    return <Navigate to={"/login"} />;
+  }
   return (
     <form className="login" onSubmit={submitForm}>
       <h1>Register</h1>
